57487880b859a3bb9c694a858bf811fcb39ef5a5
Kristiyan <kristiyan_hristov04@abv.bg>
2024-02-03 23:38:15 +0200

    Added New CSS File For Styling Videos When New Posts Are Loaded

--- SocialMedia/Controllers/HomeController.cs
+++ SocialMedia/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SocialMedia.Models;
 using System.Diagnostics;
 
 namespace SocialMedia.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
