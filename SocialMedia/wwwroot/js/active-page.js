f31092283687e6e0544bcb9f92b9424da804591b
Kristiyan <kristiyan_hristov04@abv.bg>
2024-02-15 22:05:51 +0200

    Added New JS File To Mark The Current Active Page By Applying A Background Colour In The Header

---
+++ SocialMedia/wwwroot/js/active-page.js
@@ -0,0 +1,22 @@
+﻿let url = window.location.href.toLowerCase();
+const allLinks = Array.from(document.querySelectorAll('.nav-item .nav-link'));
+let length = 0;
+
+if (allLinks.length > 5) {
+    length = 6;
+} else {
+    length = allLinks.length;
+}
+
+for (var i = 0; i < length; i++) {
+    let link = allLinks[i].href;
+
+    if (!link) {
+        continue;
+    }
+
+    let currentLinkHref = link.toLowerCase();
+    if (url === currentLinkHref) {
+        allLinks[i].style.background = '#f38f36';
+    }
+}
