eddc335e6fc3f2f4b46941ef1a834654ec09a13b
Kristiyan <kristiyan_hristov04@abv.bg>
2024-02-16 21:06:28 +0200

    Implemented Symbols Counter For Each TextArea So It's Clear How Much Space Left For Text

---
+++ SocialMedia/wwwroot/js/textarea-symbols-count.js
@@ -0,0 +1,8 @@
+﻿const maxSymbols = 450;
+const textarea = document.getElementById('textarea-symbols');
+const textareaCounter = document.getElementById('textarea-counter');
+
+textarea.addEventListener('input', () => {
+    let currentLength = textarea.value.length;
+    textareaCounter.textContent = `${currentLength} / ${maxSymbols}`;
+});
