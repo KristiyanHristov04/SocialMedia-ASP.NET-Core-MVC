330b4ab6040122b0699e894acd605615db17285f
Kristiyan <kristiyan_hristov04@abv.bg>
2024-03-10 18:35:33 +0200

    Scaffolded TwoFactorAuthentication.cshtml file And When Accessed A NotFound Status Code Is Being Returned

---
+++ SocialMedia/Areas/Identity/Pages/Account/Manage/TwoFactorAuthentication.cshtml.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+#nullable disable
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace SocialMedia.Areas.Identity.Pages.Account.Manage
+{
+    public class TwoFactorAuthenticationModel : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            return NotFound();
+        }
+    }
+}
