b69f1cde2700a91e388a6d50ad2a97f8e51dbf17
Kristiyan <kristiyan_hristov04@abv.bg>
2024-03-14 21:14:54 +0200

    When Marking The Current Active Page Ignore Query String Params

--- SocialMedia/wwwroot/js/active-page.js
+++ SocialMedia/wwwroot/js/active-page.js
@@ -1,5 +1,11 @@
 ﻿let url = window.location.href.toLowerCase();
+
+if (url.includes('?')) {
+    url = url.substring(0, url.indexOf('?'));
+}
+
 const allLinks = Array.from(document.querySelectorAll('.link-item'));
+
 
 for (var i = 0; i < allLinks.length; i++) {
     let link = allLinks[i].href;
