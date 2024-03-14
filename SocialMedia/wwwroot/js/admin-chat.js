0e9ec331964d36778bae48db08243d25ca807857
Kristiyan <kristiyan_hristov04@abv.bg>
2024-03-14 19:23:56 +0200

    Added Scroll Down Button When Admin Chat Becomes Long.

--- SocialMedia/wwwroot/js/admin-chat.js
+++ SocialMedia/wwwroot/js/admin-chat.js
@@ -1,31 +1,35 @@
-﻿let currentAdminUsername = '';
+﻿const chatContainer = document.getElementsByClassName('chat-container')[0];
+const chatIcon = document.getElementsByClassName('chat-icon')[0];
+const xMarkButton = document.getElementsByClassName('fa-xmark')[0];
+const messagesList = document.getElementById('messages-list');
+const scrollDownButton = document.getElementById('scroll-down');
 
+messagesList.addEventListener('scroll', displayButtonToScrollDown);
+scrollDownButton.addEventListener('click', scrollToBottom);
 
-var connection = new signalR.HubConnectionBuilder().withUrl("/adminChatHub").build();
-
-//Disable the send button until connection is established.
-document.getElementById("send-button").disabled = true;
-
-connection.on("ReceiveMessage", function (user, message) {
-    var li = document.createElement("li");
-    document.getElementById("messages-list").appendChild(li);
-    // We can assign user-supplied strings to an element's textContent because it
-    // is not interpreted as markup. If you're assigning in any other way, you 
-    // should be aware of possible script injection concerns.
-    li.textContent = `${user}: ${message}`;
+chatIcon.addEventListener('click', () => {
+    console.log('Clicking on Message');
+    chatContainer.classList.remove('d-none');
+    scrollToBottom();
 });
 
-connection.start().then(function () {
-    document.getElementById("send-button").disabled = false;
-}).catch(function (err) {
-    return console.error(err.toString());
+xMarkButton.addEventListener('click', () => {
+    console.log('Clicking on X');
+    chatContainer.classList.add('d-none');
 });
 
-document.getElementById("send-button").addEventListener("click", function (event) {
-    /*var user = document.getElementById("user-input").value;*/
-    var message = document.getElementById("message-input").value;
-    connection.invoke("SendMessage", "Admin", message).catch(function (err) {
-        return console.error(err.toString());
-    });
-    event.preventDefault();
-});
+function scrollToBottom() {
+    messagesList.scrollTo(0, messagesList.scrollHeight);
+}
+
+function displayButtonToScrollDown() {
+    if (messagesList.scrollTop + messagesList.clientHeight !== messagesList.scrollHeight) {
+        scrollDownButton.style.visibility = 'visible';
+    } else {
+        scrollDownButton.style.visibility = 'hidden';
+    }
+
+    //console.log('client height ' + messagesList.clientHeight);
+    //console.log('scroll height ' + messagesList.scrollHeight);
+    //console.log('scroll top ' + messagesList.scrollTop);
+}
