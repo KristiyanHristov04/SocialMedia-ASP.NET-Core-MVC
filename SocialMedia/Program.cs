4fd421370a479fc28fe6ab692f11db80f7cef959
Kristiyan <kristiyan_hristov04@abv.bg>
2024-04-04 21:10:06 +0300

    Added New Profile To Start The Application In Production

--- SocialMedia/Program.cs
+++ SocialMedia/Program.cs
@@ -73,6 +73,7 @@ namespace SocialMedia
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+                app.UseStatusCodePagesWithReExecute("/Home/Error", "?statusCode={0}");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
