d6adc78945c008547490096817f7b81fbb76872e
Kristiyan <kristiyan_hristov04@abv.bg>
2024-04-13 22:25:42 +0300

    Made Some Modifications

--- SocialMedia/Areas/Admin/Controllers/AnnouncementController.cs
+++ SocialMedia/Areas/Admin/Controllers/AnnouncementController.cs
@@ -15,7 +15,7 @@ namespace SocialMedia.Areas.Admin.Controllers
         }
 
         [HttpGet]
-        [Authorize]
+        [AllowAnonymous]
         public async Task<IActionResult> All()
         {
             List<AnnouncementViewModel> announcements
